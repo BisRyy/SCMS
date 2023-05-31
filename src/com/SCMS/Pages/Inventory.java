f5134d6da50169c4a22102770f39e4c2a5a2bbe9
BiyaMe <101469374+BiyaMe@users.noreply.github.com>
2023-05-31 07:16:08 +0300

    Update Inventory.java

--- src/com/SCMS/Pages/Inventory.java
+++ src/com/SCMS/Pages/Inventory.java
@@ -2,12 +2,62 @@ package com.SCMS.Pages;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Inventory extends JPanel{
+    private JTable inventoryTable;
+    private JScrollPane scrollPane;
 
     public Inventory(){
         setBackground(Color.LIGHT_GRAY);
-        add(new JLabel("This Is Inventory Page"));
+         String[] columnNames = {"Item", "Quantity", "Location", "Description", "Cost per item", "Inventory value"};
+        Object[][] data = {
+            {"Product A", 100, "Warehouse 1", "Something good", 300, 30000},
+            {"Product B", 50, "Warehouse 2", "Something better", 400, 20000},
+            {"Product C", 75, "Warehouse 1", "Something bad", 1000, 75000},
+            {"Product D", 120, "Warehouse 3", "Something worse", 500, 60000}
+        };
+        
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        inventoryTable = new JTable(model);
+
+        // Add a mouse listener to the table
+        inventoryTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                    int row = inventoryTable.getSelectedRow();
+                    int col = inventoryTable.getSelectedColumn();
+
+                    if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product A")) {
+                    ProductAPage productAPage = new ProductAPage();
+                    productAPage.setVisible(true);
+                    }else if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product B")) {
+                    ProductBPage productBPage = new ProductBPage();
+                    productBPage.setVisible(true);
+                    }else if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product C")) {
+                    ProductCPage productCPage = new ProductCPage();
+                    productCPage.setVisible(true);
+                    }else if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product D")) {
+                    ProductDPage productDPage = new ProductDPage();
+                    productDPage.setVisible(true);
+                    }
+            }
+        });
+
+                    // Set up the scroll pane
+                    scrollPane = new JScrollPane(inventoryTable);
+                    add(scrollPane);
+
+                    // Display the JFrame
+        setVisible(true);
     }
-    
+
+public static void main(String[] args) {
+        InventoryManagementPage inventoryPage = new InventoryManagementPage();
+        }
 }
+    
+    
+
