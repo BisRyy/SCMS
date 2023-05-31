f5113d5520537886e12bc52d28af4b6d91b9a135
Ararsa <ararsaderese6@gmail.com>
2023-05-31 13:15:26 +0300

    some features

---
+++ src/com/SCMS/customerdetails.java
@@ -0,0 +1,14 @@
+package com.SCMS;
+
+import javax.swing.JPanel;
+
+public class customerdetails extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public customerdetails() {
+
+	}
+
+}
--- src/com/SCMS/orders.java
+++ src/com/SCMS/orders.java
@@ -8,6 +8,10 @@ import java.awt.Font;
 import javax.swing.JSeparator;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.ImageIcon;
 
 public class orders extends JPanel {
 	private JTextField textField;
@@ -15,19 +19,20 @@ public class orders extends JPanel {
 		setBackground(new Color(0, 128, 128));
 		setLayout(null);
 		
+		JLabel lblNewLabel = new JLabel("Search:");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\arars\\Downloads\\search (1).png"));
+		lblNewLabel.setBounds(31, 21, 66, 39);
+		lblNewLabel.setForeground(Color.WHITE);
+		add(lblNewLabel);
+		
 		textField = new JTextField();
-		textField.setBounds(84, 24, 308, 30);
+		textField.setBounds(107, 31, 260, 29);
 		add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Search");
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(31, 10, 118, 44);
-		add(lblNewLabel);
-		
 		JPanel panel = new JPanel();
+		panel.setBounds(31, 77, 356, 159);
 		panel.setBackground(new Color(255, 255, 255));
-		panel.setBounds(31, 87, 329, 164);
 		add(panel);
 		panel.setLayout(null);
 		
@@ -94,59 +99,10 @@ public class orders extends JPanel {
 		lblNewLabel_10.setBounds(10, 0, 143, 23);
 		panel.add(lblNewLabel_10);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(255, 255, 255));
-		panel_1.setLayout(null);
-		panel_1.setBounds(31, 261, 329, 164);
-		add(panel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("QH29");
-		lblNewLabel_1_1.setBounds(10, 10, 45, 13);
-		panel_1.add(lblNewLabel_1_1);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("Customer");
-		lblNewLabel_2_1.setBounds(10, 56, 79, 13);
-		panel_1.add(lblNewLabel_2_1);
-		
-		JLabel lblNewLabel_3_1 = new JLabel("Products");
-		lblNewLabel_3_1.setBounds(10, 79, 79, 13);
-		panel_1.add(lblNewLabel_3_1);
-		
-		JLabel lblNewLabel_4_1 = new JLabel("Price");
-		lblNewLabel_4_1.setBounds(10, 102, 79, 13);
-		panel_1.add(lblNewLabel_4_1);
-		
-		JLabel lblNewLabel_5_1 = new JLabel("Delivery Status");
-		lblNewLabel_5_1.setBounds(10, 125, 79, 13);
-		panel_1.add(lblNewLabel_5_1);
-		
-		JLabel lblNewLabel_6_1 = new JLabel("Abebe");
-		lblNewLabel_6_1.setFont(new Font("Tahoma", Font.BOLD, 10));
-		lblNewLabel_6_1.setBounds(216, 56, 45, 13);
-		panel_1.add(lblNewLabel_6_1);
-		
-		JLabel lblNewLabel_7_1 = new JLabel("Beko TV");
-		lblNewLabel_7_1.setFont(new Font("Tahoma", Font.BOLD, 10));
-		lblNewLabel_7_1.setBounds(216, 79, 45, 13);
-		panel_1.add(lblNewLabel_7_1);
-		
-		JLabel lblNewLabel_8_1 = new JLabel("2800$");
-		lblNewLabel_8_1.setBounds(216, 102, 45, 13);
-		panel_1.add(lblNewLabel_8_1);
-		
-		JLabel lblNewLabel_9_1 = new JLabel("Delayed");
-		lblNewLabel_9_1.setForeground(Color.ORANGE);
-		lblNewLabel_9_1.setBounds(216, 125, 45, 13);
-		panel_1.add(lblNewLabel_9_1);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(0, 33, 329, 2);
-		panel_1.add(separator);
-		
 		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(427, 77, 324, 159);
 		panel_2.setBackground(new Color(255, 255, 255));
 		panel_2.setLayout(null);
-		panel_2.setBounds(427, 87, 329, 164);
 		add(panel_2);
 		
 		JLabel lblNewLabel_1_2 = new JLabel("QH29");
@@ -197,52 +153,10 @@ public class orders extends JPanel {
 		btnNewButton_5.setBounds(216, 143, 85, 21);
 		panel_2.add(btnNewButton_5);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(255, 255, 255));
-		panel_3.setLayout(null);
-		panel_3.setBounds(427, 261, 329, 164);
-		add(panel_3);
-		
-		JLabel lblNewLabel_1_3 = new JLabel("QH29");
-		lblNewLabel_1_3.setBounds(10, 10, 45, 13);
-		panel_3.add(lblNewLabel_1_3);
-		
-		JLabel lblNewLabel_2_3 = new JLabel("Customer");
-		lblNewLabel_2_3.setBounds(10, 56, 79, 13);
-		panel_3.add(lblNewLabel_2_3);
-		
-		JLabel lblNewLabel_3_3 = new JLabel("Products");
-		lblNewLabel_3_3.setBounds(10, 79, 79, 13);
-		panel_3.add(lblNewLabel_3_3);
-		
-		JLabel lblNewLabel_4_3 = new JLabel("Price");
-		lblNewLabel_4_3.setBounds(10, 102, 79, 13);
-		panel_3.add(lblNewLabel_4_3);
-		
-		JLabel lblNewLabel_5_3 = new JLabel("Delivery Status");
-		lblNewLabel_5_3.setBounds(10, 125, 79, 13);
-		panel_3.add(lblNewLabel_5_3);
-		
-		JLabel lblNewLabel_6_3 = new JLabel("Bisrat");
-		lblNewLabel_6_3.setBounds(216, 56, 79, 13);
-		panel_3.add(lblNewLabel_6_3);
-		
-		JLabel lblNewLabel_7_3 = new JLabel("TV");
-		lblNewLabel_7_3.setBounds(216, 79, 79, 13);
-		panel_3.add(lblNewLabel_7_3);
-		
-		JLabel lblNewLabel_8_3 = new JLabel("2800$");
-		lblNewLabel_8_3.setBounds(216, 102, 79, 13);
-		panel_3.add(lblNewLabel_8_3);
-		
-		JLabel lblNewLabel_9_3 = new JLabel("cancelled");
-		lblNewLabel_9_3.setBounds(216, 125, 79, 13);
-		panel_3.add(lblNewLabel_9_3);
-		
 		JPanel panel_2_1 = new JPanel();
+		panel_2_1.setBounds(833, 77, 329, 159);
 		panel_2_1.setBackground(new Color(255, 255, 255));
 		panel_2_1.setLayout(null);
-		panel_2_1.setBounds(833, 87, 329, 164);
 		add(panel_2_1);
 		
 		JLabel lblNewLabel_1_2_1 = new JLabel("QH29");
@@ -293,9 +207,100 @@ public class orders extends JPanel {
 		btnNewButton_8.setBounds(216, 143, 85, 21);
 		panel_2_1.add(btnNewButton_8);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(31, 241, 356, 159);
+		panel_1.setBackground(new Color(255, 255, 255));
+		panel_1.setLayout(null);
+		add(panel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("QH29");
+		lblNewLabel_1_1.setBounds(10, 10, 45, 13);
+		panel_1.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Customer");
+		lblNewLabel_2_1.setBounds(10, 56, 79, 13);
+		panel_1.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("Products");
+		lblNewLabel_3_1.setBounds(10, 79, 79, 13);
+		panel_1.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Price");
+		lblNewLabel_4_1.setBounds(10, 102, 79, 13);
+		panel_1.add(lblNewLabel_4_1);
+		
+		JLabel lblNewLabel_5_1 = new JLabel("Delivery Status");
+		lblNewLabel_5_1.setBounds(10, 125, 79, 13);
+		panel_1.add(lblNewLabel_5_1);
+		
+		JLabel lblNewLabel_6_1 = new JLabel("Abebe");
+		lblNewLabel_6_1.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblNewLabel_6_1.setBounds(216, 56, 45, 13);
+		panel_1.add(lblNewLabel_6_1);
+		
+		JLabel lblNewLabel_7_1 = new JLabel("Beko TV");
+		lblNewLabel_7_1.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblNewLabel_7_1.setBounds(216, 79, 45, 13);
+		panel_1.add(lblNewLabel_7_1);
+		
+		JLabel lblNewLabel_8_1 = new JLabel("2800$");
+		lblNewLabel_8_1.setBounds(216, 102, 45, 13);
+		panel_1.add(lblNewLabel_8_1);
+		
+		JLabel lblNewLabel_9_1 = new JLabel("Delayed");
+		lblNewLabel_9_1.setForeground(Color.ORANGE);
+		lblNewLabel_9_1.setBounds(216, 125, 45, 13);
+		panel_1.add(lblNewLabel_9_1);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(0, 33, 329, 2);
+		panel_1.add(separator);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(427, 241, 324, 159);
+		panel_3.setBackground(new Color(255, 255, 255));
+		panel_3.setLayout(null);
+		add(panel_3);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("QH29");
+		lblNewLabel_1_3.setBounds(10, 10, 45, 13);
+		panel_3.add(lblNewLabel_1_3);
+		
+		JLabel lblNewLabel_2_3 = new JLabel("Customer");
+		lblNewLabel_2_3.setBounds(10, 56, 79, 13);
+		panel_3.add(lblNewLabel_2_3);
+		
+		JLabel lblNewLabel_3_3 = new JLabel("Products");
+		lblNewLabel_3_3.setBounds(10, 79, 79, 13);
+		panel_3.add(lblNewLabel_3_3);
+		
+		JLabel lblNewLabel_4_3 = new JLabel("Price");
+		lblNewLabel_4_3.setBounds(10, 102, 79, 13);
+		panel_3.add(lblNewLabel_4_3);
+		
+		JLabel lblNewLabel_5_3 = new JLabel("Delivery Status");
+		lblNewLabel_5_3.setBounds(10, 125, 79, 13);
+		panel_3.add(lblNewLabel_5_3);
+		
+		JLabel lblNewLabel_6_3 = new JLabel("Bisrat");
+		lblNewLabel_6_3.setBounds(216, 56, 79, 13);
+		panel_3.add(lblNewLabel_6_3);
+		
+		JLabel lblNewLabel_7_3 = new JLabel("TV");
+		lblNewLabel_7_3.setBounds(216, 79, 79, 13);
+		panel_3.add(lblNewLabel_7_3);
+		
+		JLabel lblNewLabel_8_3 = new JLabel("2800$");
+		lblNewLabel_8_3.setBounds(216, 102, 79, 13);
+		panel_3.add(lblNewLabel_8_3);
+		
+		JLabel lblNewLabel_9_3 = new JLabel("cancelled");
+		lblNewLabel_9_3.setBounds(216, 125, 79, 13);
+		panel_3.add(lblNewLabel_9_3);
+		
 		JPanel panel_2_2 = new JPanel();
+		panel_2_2.setBounds(833, 241, 329, 159);
 		panel_2_2.setLayout(null);
-		panel_2_2.setBounds(833, 261, 329, 164);
 		add(panel_2_2);
 		
 		JLabel lblNewLabel_1_2_2 = new JLabel("QH29");
@@ -335,9 +340,9 @@ public class orders extends JPanel {
 		panel_2_2.add(lblNewLabel_9_2_2);
 		
 		JPanel panel_2_3 = new JPanel();
+		panel_2_3.setBounds(31, 405, 356, 159);
 		panel_2_3.setBackground(new Color(255, 255, 255));
 		panel_2_3.setLayout(null);
-		panel_2_3.setBounds(31, 435, 329, 164);
 		add(panel_2_3);
 		
 		JLabel lblNewLabel_1_2_3 = new JLabel("QH29");
@@ -378,9 +383,9 @@ public class orders extends JPanel {
 		panel_2_3.add(lblNewLabel_9_2_3);
 		
 		JPanel panel_2_4 = new JPanel();
+		panel_2_4.setBounds(427, 405, 324, 159);
 		panel_2_4.setBackground(new Color(255, 255, 255));
 		panel_2_4.setLayout(null);
-		panel_2_4.setBounds(427, 435, 329, 164);
 		add(panel_2_4);
 		
 		JLabel lblNewLabel_1_2_4 = new JLabel("QH29");
@@ -420,8 +425,8 @@ public class orders extends JPanel {
 		panel_2_4.add(lblNewLabel_9_2_4);
 		
 		JPanel panel_2_5 = new JPanel();
+		panel_2_5.setBounds(833, 405, 329, 159);
 		panel_2_5.setLayout(null);
-		panel_2_5.setBounds(833, 435, 329, 164);
 		add(panel_2_5);
 		
 		JLabel lblNewLabel_1_2_5 = new JLabel("QH29");
@@ -460,13 +465,13 @@ public class orders extends JPanel {
 		lblNewLabel_9_2_5.setBounds(216, 125, 79, 13);
 		panel_2_5.add(lblNewLabel_9_2_5);
 		
-		JButton btnNewButton_9 = new JButton("Next page");
-		btnNewButton_9.setBounds(632, 654, 124, 21);
-		add(btnNewButton_9);
-		
 		JButton btnNewButton_9_1 = new JButton("Previous Page");
-		btnNewButton_9_1.setBounds(427, 654, 124, 21);
+		btnNewButton_9_1.setBounds(427, 624, 119, 21);
 		add(btnNewButton_9_1);
+		
+		JButton btnNewButton_9 = new JButton("Next page");
+		btnNewButton_9.setBounds(632, 624, 119, 21);
+		add(btnNewButton_9);
 
 	}
 }
