220f5965a4ec56d83146530dbc62b497635c73c4
Bisrat Kebere <kiyyakebere@gmail.com>
2023-06-04 10:00:41 +0300

    :sparkles: Add Inventory Features

--- src/com/SCMS/Utils/Database.java
+++ src/com/SCMS/Utils/Database.java
@@ -40,12 +40,69 @@ public class Database {
     }
 
     public ResultSet executeQuery(String query) throws SQLException {
-        Statement statement = connection.createStatement();
+        Statement statement =  connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
         return statement.executeQuery(query);
     }
 
     public int executeUpdate(String query) throws SQLException {
         Statement statement = connection.createStatement();
         return statement.executeUpdate(query);
+    }
+
+    public Object[][] getInventory() {
+        Object[][] inventory = null;
+        try {
+            connect();
+            ResultSet resultSet = executeQuery("select * from inventory i join products p on i.product_id = p.product_id join suppliers s on p.supplier_id = s.supplier_id;");
+            int rowCount = getRowCount(resultSet);
+            int columnCount = 11;
+            inventory = new Object[rowCount][columnCount];
+            resultSet.beforeFirst();
+            int i = 0;
+            while (resultSet.next()) {
+
+                inventory[i][0] = resultSet.getString("code");
+                inventory[i][1] = resultSet.getString("name");
+                inventory[i][2] = resultSet.getInt("quantity");
+                inventory[i][3] = resultSet.getDouble("price");
+                inventory[i][4] = resultSet.getString("unit");
+                inventory[i][5] = resultSet.getString("category");
+                inventory[i][6] = resultSet.getString("supplier_name");
+                inventory[i][7] = resultSet.getString("date_added");
+                inventory[i][8] = resultSet.getString("expiry_date");
+                inventory[i][9] = resultSet.getString("description");
+                inventory[i][10] = resultSet.getString("product_id");
+                i++;
+            }
+            disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return inventory;
+    }
+
+    private int getRowCount(ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.last();
+                return resultSet.getRow();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public boolean addInventory(int product_id, int quantity, String location, String expiry_date) {
+        String query = "insert into inventory(product_id, quantity, location, expiry_date) values(" + product_id + ", " + quantity + ", '" + location + "', '" + expiry_date + "');";
+        try{
+            connect();
+            executeUpdate(query);
+            disconnect();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- src/com/SCMS/Utils/SCMS.sql
+++ src/com/SCMS/Utils/SCMS.sql
@@ -12,23 +12,26 @@ CREATE TABLE users(
     role varchar(25)
 );
 
-CREATE TABLE product(
-    product_id int primary key auto_increment,
-    product_name varchar(25),
-    product_price int,
-    product_description varchar(25),
-    product_image varchar(25),
-    product_category varchar(25),
-    product_quantity int,
-    product_status varchar(25)
+CREATE TABLE products (
+  product_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  code VARCHAR(50) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  unit VARCHAR(50),
+  category VARCHAR(100),
+  description VARCHAR(255),
+  image VARCHAR(255),
+  supplier_id INT
 );
 
-CREATE TABLE inventory(
-    inventory_id int primary key auto_increment,
-    product_id int,
-    product_quantity int,
-    product_status varchar(25),
-    foreign key(product_id) references product(product_id)
+CREATE TABLE inventory (
+  inventory_id INT PRIMARY KEY AUTO_INCREMENT,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  location VARCHAR(100),
+  date_added DATE default CURRENT_TIMESTAMP,
+  expiry_date DATE,
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
 CREATE TABLE suppliers(
@@ -46,6 +49,29 @@ CREATE TABLE orders(
     order_quantity int,
     order_status varchar(25),
     foreign key(user_id) references users(user_id),
-    foreign key(product_id) references product(product_id)
+    foreign key(product_id) references products(product_id)
 );
+
+ALTER TABLE products ADD FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id);
+
+
+INSERT INTO products (name, code, price, unit, category, description, image, supplier_id) VALUES
+('Product 1', 'P1', 10.00, 'Unit', 'Category', 'Description', 'Image', 1),
+('Product 2', 'P2', 20.00, 'Unit', 'Category', 'Description', 'Image', 1),
+('Product 3', 'P3', 30.00, 'Unit', 'Category', 'Description', 'Image', 2),
+('Product 4', 'P4', 40.00, 'Unit', 'Category', 'Description', 'Image', 2);
+
+INSERT INTO inventory (product_id, quantity, location, expiry_date) VALUES
+(1, 50, 'Warehouse A', '2021-05-31'),
+(2, 30, 'Warehouse B', '2023-05-31'),
+(3, 20, 'Warehouse A', NULL),
+(4, 40, 'Warehouse C', 2022-05-31);
+
+INSERT INTO suppliers (supplier_name, supplier_address, supplier_phone, supplier_email) VALUES
+('Supplier 1', 'Address 1', '1234567890', 'supplier@supply.me'),
+('Supplier 2', 'Address 2', '0987654321', 'supplier2@supply.me');
+
+INSERT INTO users (username, password, email, phone, address, role) VALUES
+('admin', 'admin', 'admin', 'admin', 'admin', 'Administrator'),
+('user', 'user', 'user', 'user', 'user', 'Employee');
 
